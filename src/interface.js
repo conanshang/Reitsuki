0254b065c2c663b5eb885a3db85845666d2b3b63
conanshang <conanshang@gmail.com>
2013-09-18 00:52:27 -0400

    Added some assets.

---
+++ src/interface.js
@@ -0,0 +1,12 @@
+// User changable data.
+var interfaceWidth = 1280, interfaceHeight = 800;
+
+
+// Adjust the window.
+$('#interface_window').css({
+	"width" : interfaceWidth,
+	"height" : interfaceHeight,
+	
+	"left" : ($(window).width() - interfaceWidth) / 2,
+});
+
